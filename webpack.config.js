f7d134033aeab2a2b723d8bb36dabb9135c20964
Albertina75 <145276835+Albertina75@users.noreply.github.com>
2024-08-10 02:07:46 +0200

    just playing

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,11 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
-module.exports = {
-  mode: 'development', // Cambia esto a 'production' para construir para producción
+export default {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    path: path.resolve('dist'),
+    filename: 'bundle.js',
   },
   module: {
     rules: [
@@ -16,20 +15,20 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
-      }
-    ]
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html'
-    })
+      template: './src/index.html',
+    }),
   ],
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 9000
-  }
+  mode: 'development', // O 'production' dependiendo de tu entorno
 };
