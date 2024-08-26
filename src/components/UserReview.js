4115f5776fa945bd7d3c9fbbfbc27cb1a162a7a3
Albertina75 <145276835+Albertina75@users.noreply.github.com>
2024-08-26 14:14:13 +0200

    Fix: Extensions

--- src/components/UserReview.js
+++ src/components/UserReview.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import UserReview from './components/UserReview';
-import ReviewForm from './components/ReviewForm'; // Corregido el nombre de importación
+import UserReview from './components/UserReview.js';
+import ReviewForm from './components/ReviewForm.js'; // Corregido el nombre de importación
 
 const UserReviews = ({ placeId }) => {
   const [reviews, setReviews] = useState([]);
@@ -12,7 +12,7 @@ const UserReviews = ({ placeId }) => {
   }, [placeId]);
 
   const addReview = (review) => {
-    console.log("Nuevo review añadido:", review);
+    console.log("Nuevo review añadido:", review.js);
     setReviews([...reviews, review]);
   };
 
--- webpack.config.cjs
+++ webpack.config.cjs
@@ -36,6 +36,6 @@ module.exports = {
   devServer: {
     static: join(__dirname, 'dist'),
     compress: true,
-    port: 19980,
+    port:5344,
   },
 };
